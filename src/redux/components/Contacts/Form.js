1635b3ff2dae5026fe253970018fbd08f708ef4c
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-08 02:50:48 +0300

    Form.js created.

---
+++ src/redux/components/Contacts/Form.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function Form() {
+    const [name, setName] = useState('');
+
+    return (
+        <div>
+            <form>
+                <input value={name} onChange={(e) => setName(e.target.value)} />
+            </form>
+        </div>
+    )
+}
+
+export default Form
