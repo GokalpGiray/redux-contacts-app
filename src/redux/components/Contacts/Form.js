f7e7c1a02c1079b28a37f5b0b39f9755ea204b0e
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-08 18:38:45 +0300

    Added phone number feature and styling.

--- src/redux/components/Contacts/Form.js
+++ src/redux/components/Contacts/Form.js
@@ -5,23 +5,29 @@ import { addContact } from '../../contactSlice';
 
 function Form() {
     const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
     const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if (!name || name === " ") return false;
+        if (!name || name === " " || !number || number === " ") return false;
 
-        dispatch(addContact({ id: nanoid(), name }));
+        dispatch(addContact({ id: nanoid(), name, phoneNumber: number }));
 
         setName('');
+        setNumber('');
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+                <input placeholder='Phone Number' value={number} onChange={(e) => setNumber(e.target.value)} />
+                <div className="btn">
+                    <button type='submit' >Add</button>
+                </div>
             </form>
         </div>
     )
