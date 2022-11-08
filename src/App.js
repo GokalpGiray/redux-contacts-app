ab5741ed09a0fe642efba8bd1df9aa10e0d10d84
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-09 00:28:02 +0300

    Error page has been added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Contacts from './redux/components/Contacts';
 import Edit from './redux/components/Contacts/Edit';
+import Error from './redux/components/Error';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Contacts />} />
             <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Error />} />
           </Routes>
         </Router>
       </div>
---
+++ src/redux/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+      <h3>Error: This route was not found.</h3>
+    </div>
+  )
+}
+
+export default Error
