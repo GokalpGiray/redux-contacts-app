17c87595c86f87ecf1d71ce962a4413e970271cd
Gokalp Giray <gokalpgiray24@gmail.com>
2022-11-08 21:41:10 +0300

    Delete Button has been added.

--- src/redux/components/Contacts/Item.js
+++ src/redux/components/Contacts/Item.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../contactSlice';
 
 function Item({ item }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    if(window.confirm("Are you sure?")) {
+      dispatch(deleteContact(id))
+    };
+  };
+
   return (
     <li>
       <span>{item.name}</span>
       <span>{item.phoneNumber}</span>
+      <span className='deleteBtn' onClick={() => handleDelete(item.id)} >X</span>
     </li>
   )
 }
